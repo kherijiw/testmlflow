1bfa6841efa2cc09133b463d141f1c05c75a3845
Walid Kheriji <walid.kheriji@vedecom.fr>
2025-11-25 17:06:56 +0100

    ajout de mlfow

--- src/README.md
+++ src/README.md
@@ -1,7 +1,17 @@
 
-# install env
-'''bash
+# install environement sous unix 
+```bash
+pip install pipenv
+pipenv install 
+pipenv shell
+chmod u+x pipeline.sh
+```
+
+# install environement sous wind
+
+```powershell
 pip install pipenv
 pipenv install 
 pipenv shell
 ./pipeline.bat
+```
--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import joblib
+import mlflow
 
 ###########################################################
 # 4. ENTRAINEMENT DU MODELE AVEC PIPELINE
@@ -13,15 +14,19 @@ def train_model(X_train, y_train):
     # pipeline = standardisation + random forest
     pipeline = Pipeline([
         ("scaler", StandardScaler()),
-        ("clf", RandomForestClassifier(n_estimators=200, random_state=42))
+        ("clf", RandomForestClassifier(n_estimators=150, random_state=42))
     ])
 
     pipeline.fit(X_train, y_train)
     return pipeline
 
 if __name__ == "__main__":
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("Iris")
+
     iris = pd.read_csv("./data/iris.csv")
     X_train, X_test, y_train, y_test = prepare_data(iris)
     print("3️⃣ Entraînement du modèle...")
+    mlflow.sklearn.autolog()
     model = train_model(X_train, y_train)
     joblib.dump(model, "./model/iris_model.joblib")
